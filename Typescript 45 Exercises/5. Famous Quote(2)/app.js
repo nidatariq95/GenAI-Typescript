8dd9bf4734bb6ae5aaa6ea5487921888d01da0f5
nidatariq95 <128402335+nidatariq95@users.noreply.github.com>
2024-02-26 01:47:00 +0500

    Create app.js

---
+++ Typescript 45 Exercises/5. Famous Quote(2)/app.js
@@ -0,0 +1,4 @@
+var quote = "The most valuable of all talents is that of never using two words when one will do.";
+var famous_person = "Thomas Jefferson";
+var message = "".concat(famous_person, " once said, ").concat(quote);
+console.log(message);
